19a80782ff88bcf84192963e210389a091e71cb0
Ji Zhang <zhangji0810@gmail.com>
2021-03-08 15:58:11 +0100

    some changes to Progress Steps

--- Progress_Steps/script.js
+++ Progress_Steps/script.js
@@ -28,11 +28,11 @@ function update() {
         } else {
             circle.classList.remove('active');
         }
-    });
+    }); // Add or remove active class to each circle. idx means index
 
     const actives = document.querySelectorAll('.active');
 
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%' // Adjust the leangth of the blue line
 
     if(currentActive === 1) {
         previous.disabled = true;
@@ -41,6 +41,6 @@ function update() {
     } else {
         previous.disabled = false;
         next.disabled = false;
-    }
+    } // button active or not
 }
 
